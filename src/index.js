8adb7d2656d398d32ea2b081aa3f2e3f1ff30179
faisalsayed10 <faisal.sayed502@gmail.com>
2020-12-15 13:37:42 +0530

    refactoring README.md

--- src/index.js
+++ src/index.js
@@ -6,9 +6,6 @@ import * as serviceWorker from './serviceWorker';
 import { GithubProvider } from './context/context';
 import { Auth0Provider } from '@auth0/auth0-react';
 
-// DOMAIN NAME - dev-76hl5x2u.au.auth0.com
-// Client ID - uBa2CfWFG4tgESy7Y394Bw9md70Hq2Zh
-
 ReactDOM.render(
   <React.StrictMode>
     <Auth0Provider
